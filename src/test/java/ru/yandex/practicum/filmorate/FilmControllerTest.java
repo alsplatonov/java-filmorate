2f77c6bea1a972caece17df27bca0c9a06e87b7d
Alexander Platonov <platon1560@gmail.com>
2026-02-03 23:10:23 +0300

    add tests for user controller, film controller

---
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -0,0 +1,77 @@
+package ru.yandex.practicum.filmorate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.yandex.practicum.filmorate.controller.FilmController;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(FilmController.class)
+class FilmControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private Film validFilm;
+
+    @BeforeEach
+    void setUp() {
+        validFilm = new Film();
+        validFilm.setName("Test Film");
+        validFilm.setDescription("Описание фильма");
+        validFilm.setReleaseDate(LocalDate.of(2000, 1, 1));
+        validFilm.setDuration(120);
+    }
+
+    @Test
+    void createFilm_EmptyRequestBody_ReturnsBadRequest() throws Exception {
+        mockMvc.perform(post("/films")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("")) // пустой JSON
+                .andExpect(status().isBadRequest());
+    }
+
+
+    @Test
+    void createFilm_ValidFilm_ReturnsCreatedFilm() throws Exception {
+        mockMvc.perform(post("/films")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(validFilm)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.name").value("Test Film"));
+    }
+
+    @Test
+    void createFilm_EmptyName_ReturnsBadRequest() throws Exception {
+        validFilm.setName("");
+
+        mockMvc.perform(post("/films")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(validFilm)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createFilm_NegativeDuration_ReturnsBadRequest() throws Exception {
+        validFilm.setDuration(-10);
+
+        mockMvc.perform(post("/films")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(validFilm)))
+                .andExpect(status().isBadRequest());
+    }
+}
