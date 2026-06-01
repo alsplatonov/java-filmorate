b9dc8c20392ee65696ddb1bbb192223ef62c4a47
vadim_gotovec <vadimgotovec1@gmail.com>
2026-06-01 21:04:46 +0300

    Исправления после git-теста

--- src/main/java/ru/yandex/practicum/filmorate/dao/EventDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/EventDbStorage.java
@@ -6,13 +6,14 @@ import ru.yandex.practicum.filmorate.model.Event;
 import ru.yandex.practicum.filmorate.model.EventType;
 import ru.yandex.practicum.filmorate.model.Operation;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
 public class EventDbStorage extends BaseRepository<Event> implements EventStorage {
 
     private static final String INSERT_QUERY =
-            "INSERT INTO events(user_id, event_type, operation, entity_id, timestamp) VALUES (?, ?, ?, ?, ?)";
+            "INSERT INTO events(user_id, event_type, operation, entity_id, created_at) VALUES (?, ?, ?, ?, ?)";
 
     private static final String FIND_BY_USER_ID_QUERY =
             "SELECT * FROM events WHERE user_id = ? ORDER BY event_id ASC";
@@ -25,20 +26,28 @@ public class EventDbStorage extends BaseRepository<Event> implements EventStorag
             event.setEventType(EventType.valueOf(rs.getString("event_type")));
             event.setOperation(Operation.valueOf(rs.getString("operation")));
             event.setEntityId(rs.getLong("entity_id"));
-            event.setTimestamp(rs.getLong("timestamp"));
+
+            // Читаем из БД как Timestamp и конвертируем обратно в число Long
+            Timestamp createdAt = rs.getTimestamp("created_at");
+            if (createdAt != null) {
+                event.setTimestamp(createdAt.getTime());
+            }
+
             return event;
         });
     }
 
     @Override
     public Event create(Event event) {
+        Timestamp sqlTimestamp = new Timestamp(event.getTimestamp());
+
         long id = insert(
                 INSERT_QUERY,
                 event.getUserId(),
                 event.getEventType().name(),
                 event.getOperation().name(),
                 event.getEntityId(),
-                event.getTimestamp()
+                sqlTimestamp
         );
         event.setEventId(id);
         return event;
